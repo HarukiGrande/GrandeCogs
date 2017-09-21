403c15efd02ce50cdaefd26ae072eb3ccf9c5131
Mr Boo Grande <jordan99oliver@outlook.com>
2017-09-21 15:08:26 +0100

    Update radioharu.py

--- radioharu/radioharu.py
+++ radioharu/radioharu.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from cogs.utils import checks
 from __main__ import send_cmd_help
+import asyncio
 
 class RadioHaru:
     """Radio Haru"""
@@ -22,21 +23,34 @@ class RadioHaru:
         server = ctx.message.server
         author = ctx.message.author
         if self.voice_connected(server):
-            await self.bot.say("Already connected to a voice channel, do `{}stop` to change radio.".format(ctx.prefix))
+            await self.bot.say("Already connected to a voice channel, use `{}radioharu stop` to change radio.".format(ctx.prefix))
         else:
             voice_channel = author.voice_channel
             voice = await self.bot.join_voice_channel(voice_channel)
+            Channel = ctx.message.channel
+            await self.bot.send_typing(Channel)
+            player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg')
+            player.start()
+            await asyncio.sleep(7)
+            player.stop()
             player = voice.create_ffmpeg_player('http://stream.radioharu.pw/owo')
             player.start()
-            await self.bot.say("**Playing Radio Haru!**")
+            await self.bot.say(":green_heart: **Playing Radio Haru!**")
 
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
     async def stop(self, ctx):
         """Stop Radio Haru"""
         server = ctx.message.server
+        author = ctx.message.author
         await self._disconnect_voice_client(server)
-        await self.bot.say("**Stopped playing Radio!**")
+        voice_channel = author.voice_channel
+        voice = await self.bot.join_voice_channel(voice_channel)
+        player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360425539309142037/radioharu_goodbye.mp3')
+        player.start()
+        await asyncio.sleep(1)
+        await self._disconnect_voice_client(server)
+        await self.bot.say(":red_circle: **Stopped playing Radio!**")
         
     def voice_client(self, server):
         return self.bot.voice_client_in(server)
