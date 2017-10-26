76ca2a43778416469129fffbd9beae006724aefd
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-10-26 09:45:46 -0400

    Added MOST edits, I need a tester pls

--- radioharu/radioharu.py
+++ radioharu/radioharu.py
@@ -9,6 +9,7 @@ class RadioHaru:
 
     def __init__(self, bot):
         self.bot = bot
+        self.use_avconv = self.bot.get_cog("Audio").settings["AVCONV"]
     
     @commands.group(pass_context=True, no_pm=True)
     async def radioharu(self, ctx):
@@ -19,39 +20,44 @@ class RadioHaru:
 
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
-    async def play(self, ctx):
+    async def play(self, ctx, voice_channel: discord.Channel=None):
         """Play Radio Haru"""
         server = ctx.message.server
         author = ctx.message.author
+        if voice_channel == None:
+            voice_channel = author.voice_channel
         if self.voice_connected(server):
             await self.bot.say("Already connected to a voice channel, use `{}radioharu stop` to change radio.".format(ctx.prefix))
         else:
-            voice_channel = author.voice_channel
-            voice = await self.bot.join_voice_channel(voice_channel)
-            Channel = ctx.message.channel
-            await self.bot.send_typing(Channel)
-            player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg')
-            player.start()
-            await asyncio.sleep(7)
-            player.stop()
-            player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo')
-            player.start()
-            await self.bot.say(":green_heart: **Playing Radio Haru!**")
+            try:
+                voice = await self.bot.join_voice_channel(voice_channel)
+                Channel = ctx.message.channel
+                await self.bot.send_typing(Channel)
+                player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg', use_avconv=self.use_avconv)
+                player.start()
+                await asyncio.sleep(7)
+                player.stop()
+                player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo', use_avconv=self.use_avconv)
+                player.start()
+                await self.bot.say(":green_heart: **Playing Radio Haru!**")
+            except discord.ext.commands.errors.CommandInvokeError:
+                await self.bot.say("You either didn't enter a voice channel to connect to, or weren't in one!")
             
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
-    async def playwin(self, ctx):
+    async def playwin(self, ctx, voice_channel: discord.Channel=None):
         """Play Radio Haru - Use instead of play command if on a Windows machine."""
         server = ctx.message.server
         author = ctx.message.author
         if self.voice_connected(server):
             await self.bot.say("Already connected to a voice channel, use `{}radioharu stop` to change radio.".format(ctx.prefix))
         else:
-            voice_channel = author.voice_channel
+            if voice_channel == None:
+                voice_channel = author.voice_channel
             voice = await self.bot.join_voice_channel(voice_channel)
             Channel = ctx.message.channel
             await self.bot.send_typing(Channel)
-            player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg')
+            player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg', use_avconv=self.use_avconv)
             player.start()
             await asyncio.sleep(7)
             player.stop()
@@ -59,11 +65,14 @@ class RadioHaru:
             await self.bot.say(":green_heart: **Playing Radio Haru!**")
             radio = True
             while radio == True:
-                voice = await self.bot.join_voice_channel(voice_channel)
-                player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo')
-                player.start()
-                await asyncio.sleep(300)
-                await self._disconnect_voice_client(server)
+                try:
+                    voice = await self.bot.join_voice_channel(voice_channel)
+                    player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo', use_avconv=self.use_avconv)
+                    player.start()
+                    await asyncio.sleep(300)
+                    await self._disconnect_voice_client(server)
+                except discord.ext.commands.errors.CommandInvokeError:
+                    await self.bot.say("You either didn't enter a voice channel to connect to, or weren't in one!")
 
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
@@ -83,6 +92,7 @@ class RadioHaru:
     @radioharu.command(pass_context=True, no_pm=True)
     async def donate(self, ctx):
         """Donate for server costs"""
+        await self.bot.say("Running this amazing music server is not cheap, so please feel free to throw some money my way to help pay for server costs")
         await self.bot.say("**https://patreon.com/harukigrande/**")
         
     def voice_client(self, server):
